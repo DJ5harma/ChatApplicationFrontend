5c96fcfcfe0fe7b750fe642586f5cc6c65cc3ca4
DJ5harma <sunubhargav2004@gmail.com>
2024-04-07 00:51:25 +0530

    Changing ws:// to wss:// to hopefully resolve the DOM exception issue

--- src/Utilities/HardCodedData.ts
+++ src/Utilities/HardCodedData.ts
@@ -1,5 +1,9 @@
+// export const WEB_SOCKET_URL = "ws://localhost:3000";
+
+// export const API_URL = "http://localhost:3000/api";
+
 export const WEB_SOCKET_URL =
-	"ws://chatapplicationbackend-production-a994.up.railway.app";
+	"wss://chatapplicationbackend-production-a994.up.railway.app";
 
 export const API_URL =
 	"https://chatapplicationbackend-production-a994.up.railway.app/api";
