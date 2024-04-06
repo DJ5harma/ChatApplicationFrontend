5899581cb1305b56c7016a41c2041e192ff48fbb
DJ5harma <sunubhargav2004@gmail.com>
2024-04-07 00:04:20 +0530

    Deployed server testing

--- src/Utilities/HardCodedData.ts
+++ src/Utilities/HardCodedData.ts
@@ -1,3 +1,5 @@
-export const WEB_SOCKET_URL = "ws://localhost:3000";
+export const WEB_SOCKET_URL =
+	"ws://chatapplicationbackend-production-a994.up.railway.app";
 
-export const API_URL = "http://localhost:3000/api";
+export const API_URL =
+	"https://chatapplicationbackend-production-a994.up.railway.app/api";
