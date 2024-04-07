13b26d9d8e77c8e2d659ad0c0fb6d31330f2137a
Dhananjay Bhargav Sharma <138970319+DJ5harma@users.noreply.github.com>
2024-04-08 01:17:29 +0530

    Update UserProvider.tsx
    removed cookie-related code as now localStorage is the homie

--- src/contexts/User/UserProvider.tsx
+++ src/contexts/User/UserProvider.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { ReactNode, createContext, useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
 import { UserContextType } from "../../Utilities/DataTypes";
 
 export const UserContext = createContext<UserContextType>({
@@ -18,25 +17,7 @@ export default function UserProvider({ children }: { children: ReactNode }) {
 	const [username, setUsername] = useState("");
 	const [_id, setId] = useState("");
 	const [loggedIn, setLoggedIn] = useState(false);
-	const [loading, setLoading] = useState(true);
-	const [cookies] = useCookies(["token"]);
-
-	useEffect(() => {
-		const { token } = cookies;
-
-		if (!token) setLoading(false);
-		else
-			(async () => {
-				const { data } = await axios.post(`/auth/Wall`, {
-					autoLogin: true,
-				});
-				if (data.error) return;
-				setLoggedIn(true);
-				setUsername(data.username);
-				setId(data._id);
-				setLoading(false);
-			})();
-	}, []);
+	const [loading, setLoading] = useState(false);
 
 	return (
 		<UserContext.Provider
