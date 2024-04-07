521fb17e9eec15d536da4b9d4c9ffedf2aa227ba
Dhananjay Bhargav Sharma <138970319+DJ5harma@users.noreply.github.com>
2024-04-08 01:40:20 +0530

    Update Loading.tsx

--- src/Pages/Loading.tsx
+++ src/Pages/Loading.tsx
@@ -6,11 +6,13 @@ export default function Loading() {
 				width: "100vw",
 				backgroundColor: "black",
 				display: "flex",
+    flexDirection:"column",
 				justifyContent: "center",
 				alignItems: "center",
 			}}
 		>
 			<h1>Loading...</h1>
+   <p>Please reload the page if it takes too long...</p>
 		</div>
 	);
 }
