979e7843be3e5edffe149a576e232475c0297a23
Dhananjay Bhargav Sharma <138970319+DJ5harma@users.noreply.github.com>
2024-04-09 00:44:53 +0530

    Update InputSection.tsx

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -21,6 +21,7 @@ export default function InputSection({ wss }: { wss: WebSocket }) {
 			/>
 			<button
 				onClick={() => {
+     if(message==="") return;
 					wss.send(
 						JSON.stringify({
 							type: "sendMessage",
