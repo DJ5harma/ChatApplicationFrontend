ac7cf4757efa9a190e8dad08763faba6d262c300
DJ5harma <sunubhargav2004@gmail.com>
2024-04-21 12:45:48 +0530

    now using envVars instead of HardCoded data

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,22 @@ import Home from "./Pages/Home";
 import Loading from "./Pages/Loading";
 
 import UserProvider from "./contexts/User/UserProvider";
-
-import { API_URL, WEB_SOCKET_URL } from "./Utilities/HardCodedData";
 import DataProvider from "./contexts/Data/DataProvider";
+
 function App() {
+	const { VITE_WEBSOCKET_URL, VITE_HTTP_URL } = import.meta.env;
+
 	const [wss, setWss] = useState<WebSocket>(); // defining a variable wss to store the websocket conection that we'll establish in the useEffect hook
 
 	useEffect(() => {
-		const WebSocketServerConnection = new WebSocket(WEB_SOCKET_URL); // a new local variable is now making and storing the web socket server connection
+		const WebSocketServerConnection = new WebSocket(VITE_WEBSOCKET_URL); // a new local variable is now making and storing the web socket server connection
 		setWss(WebSocketServerConnection); // we set our wss variable to hold this connection so that we can pass it on to other components
 	}, []);
 
-	axios.defaults.baseURL = API_URL; // Telling axios to always refer requests with our HardCoded URL as prefix
+	axios.defaults.baseURL = VITE_HTTP_URL; // Telling axios to always refer requests with our HardCoded URL as prefix
 	// axios.defaults.withCredentials = true;
 
 	if (!wss) return <Loading />;
-
 	// if everything checks out, we'll render the
 	return (
 		<UserProvider wss={wss}>
--- src/Utilities/HardCodedData.ts
+++
@@ -1,9 +0,0 @@
-// export const WEB_SOCKET_URL = "ws://localhost:3000";
-
-// export const API_URL = "http://localhost:3000/api";
-
-export const WEB_SOCKET_URL =
-	"wss://chatapplicationbackend-production-a994.up.railway.app";
-
-export const API_URL =
-	"https://chatapplicationbackend-production-a994.up.railway.app/api";
